1b24cc1ef29c570a272ca9b4311d3ce003424229
Gabriela2001 <rokagabriela@gmail.com>
2019-05-28 15:03:45 -0600

    primero

--- pokeApi/js/script.js
+++ pokeApi/js/script.js
@@ -1,9 +1,11 @@
 var p = angular.module("p",[])
 p.controller("PokeCtrl",function($scope,$rootScope,$http){
 
-	$scópe.Pokedex={},
+	$scope.Pokedex={},
 	$http({
 		method:"GET",
 		url:"https://pokeapi.co/api/v2/"
-	}).then(function Datos(x))
+	}).then(function Datos(x){
+		console.log(x);
+	})
 })
